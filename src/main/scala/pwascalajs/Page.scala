fdba05221d713da9f4c192ba97602d72c16fbee9
Henri Muurimaa <henri@vaadin.com>
2016-12-19 16:31:03 +0200

    react to on/offline

--- src/main/scala/pwascalajs/Page.scala
+++ src/main/scala/pwascalajs/Page.scala
@@ -1,11 +1,27 @@
 package pwascalajs
 
+import org.scalajs.dom
+import scala.concurrent.ExecutionContext.Implicits.global
+import scala.util.{Failure, Success}
+
 object Page {
   def hideOfflineWarning() {
     println("Hiding offline warning")
+    val e = dom.document.getElementById("offline")
+    dom.document.body.removeChild(e)
   }
 
   def showOfflineWarning() {
     println("Showing offline warning")
+
+    // load html template informing the user they are offline
+    dom.ext.Ajax.get("./offline.html").onComplete {
+      case Success(xhr) =>
+        val e = dom.document.createElement("div")
+        e.setAttribute("id", "offline")
+        e.innerHTML = xhr.responseText
+        dom.document.body.appendChild(e)
+      case Failure(_) => println(s"Failed to load grid data.")
+    }
   }
 }
